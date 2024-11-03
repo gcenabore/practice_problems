24f96bba8ffdc26d72d8ea50e36a82ea223cb230
gcenabore <gianenabore30@gmail.com>
2024-11-03 19:58:01 +0800

    pseudocode or plan

---
+++ grade_remarks.py
@@ -0,0 +1,6 @@
+#use if-else statement
+#type a desired grade 50-100, 98 GRADE is equivalent to 1.0, 75 GRADE is equivalent to 3.0, 74 grade is equivalent to 5.0
+#if GRADE is >= 75, print "PASSED", otherwise print "FAILED, study hard"
+#ask the user for input
+# print the result
+
