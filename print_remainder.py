6aa9cd6ac14f126b51bacc7f2b806777b822a73e
gcenabore <gianenabore30@gmail.com>
2024-11-05 23:19:20 +0800

    first commit: first try

---
+++ print_remainder.py
@@ -0,0 +1,8 @@
+#10. Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+num1 = int(input("Please enter a number: "))
+
+num2 = int(input("Please enter another number: "))
+
+remainder = num1 % num2 
+print(f"the remainder of {num1} and {num2} is {remainder} okey ba yun? ehhehe")
