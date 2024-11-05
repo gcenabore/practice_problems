c3f51fe066bf38b25faead22f3e61a453d6345de
gcenabore <gianenabore30@gmail.com>
2024-11-05 23:09:51 +0800

    first commit: furst try

---
+++ print_quotient_nodecimal.py
@@ -0,0 +1,8 @@
+#8. Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+num1 = int(input("Please enter a number: "))
+num2 = int(input("Please enter another number: "))
+
+quotient = num1 // num2 
+
+print(f"the quotient of {num1} and {num2} is {quotient} mhuehehe ayan na ha no decimal 'yan guys~")
