ae8957d453046319980d91a42a346495b78f933e
gcenabore <gianenabore30@gmail.com>
2024-11-05 22:58:50 +0800

    first commit: first try

---
+++ print_difference.py
@@ -0,0 +1,8 @@
+#6. Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+num1 = float(input("Please enter a number: "))
+num2 = float(input("Please enter another number: "))
+
+difference = num1 - num2
+
+print(f"the difference of {num1} and {num2} is {difference}. mhuehehwahha skibidi bomni")
