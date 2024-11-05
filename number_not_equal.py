a2e5c018d24503c3633f31e5387e4c8d5d6df175
gcenabore <gianenabore30@gmail.com>
2024-11-05 22:47:30 +0800

    first commit: first try

---
+++ number_not_equal.py
@@ -0,0 +1,10 @@
+#4. Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+input1 = int(input("PLease enter a number: "))
+input2 = int(input("Please enter another number: "))
+
+if input1 != input2:
+    print(f"{input1} and {input2} is not equal hehehehehe.")
+
+else:
+    print(f"{input1} is equal to {input2} hwhahahaha.")
