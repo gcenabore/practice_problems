f6817f0b371bf1abdc7fc4dd4298509e63795141
gcenabore <gianenabore30@gmail.com>
2024-11-05 22:31:47 +0800

    first commit: first try

---
+++ print_equal_numbers.py
@@ -0,0 +1,11 @@
+#Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+input1 = int(input("Please input a number: "))
+
+input2 = int(input("Please input another number: "))
+
+if input1 == input2:
+    print(f"{input1} and {input2} is equal, yehey! :]")
+
+else:
+    print(f"{input1} and {input2} is not equal po! :[")
