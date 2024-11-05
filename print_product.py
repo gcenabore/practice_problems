de7d49cae24afe4112aeb0aceea47dbe219635af
gcenabore <gianenabore30@gmail.com>
2024-11-05 23:01:28 +0800

    first commit: first try

---
+++ print_product.py
@@ -0,0 +1,8 @@
+#7. Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+num1 = float(input("Please enter a number: "))
+num2 = float(input("Please enter another number: "))
+
+product = num1 * num2
+
+print(f"the product of {num1} and {num2} is {product} po.. okaeys")
