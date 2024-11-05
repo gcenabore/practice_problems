5c34bde5bcc2c69b55b9e82bc933a29160dc42c0
gcenabore <gianenabore30@gmail.com>
2024-11-05 22:55:18 +0800

    first commit: first try

---
+++ print_sum.py
@@ -0,0 +1,9 @@
+#5. Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+number1 = float(input("Please enter a number: "))
+
+number2 = float(input("Please enter another number: "))
+
+sum = number1 + number2
+
+print(f"the sum of {number1} and {number2} is {sum} mhuehehhee.")
