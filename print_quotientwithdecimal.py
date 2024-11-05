c23ab79732f0fda258d0dc0ecdef7f0e8650054d
gcenabore <gianenabore30@gmail.com>
2024-11-05 23:12:31 +0800

    first commit: fisrt try

---
+++ print_quotientwithdecimal.py
@@ -0,0 +1,9 @@
+#9. Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+num1 = int(input("Please enter a number: "))
+
+num2 = int(input("Please enter another number: "))
+
+quotient = num1 / num2
+
+print(f"the quotient of {num1} and {num2} is {quotient} ayan po ang sagot nang wmay decimal na mhuwaahehehahhsekibidiii")
